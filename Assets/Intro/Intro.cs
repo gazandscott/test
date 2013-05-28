a035c005ddb9f45f53a8b36ee8241c763207bc27
Gary Buyn <simplegsb@gmail.com>
2013-05-28 18:31:59 +1200

    Added proper intro pics.

--- Assets/Intro/Intro.cs
+++ Assets/Intro/Intro.cs
@@ -6,6 +6,7 @@ public class Intro : MonoBehaviour
 	public Material introMaterial1;
 	public Material introMaterial2;
 	public Material introMaterial3;
+	public Material introMaterial4;
 	
 	int introSlide;
 	
@@ -16,19 +17,26 @@ public class Intro : MonoBehaviour
 		{
 			introSlide++;
 			GameObject plane = GameObject.Find("Plane");
+			Material[] materials = plane.renderer.materials;
 			
 			if (introSlide == 2)
 			{
-				plane.renderer.material = introMaterial2;
+				materials[1] = introMaterial2;
 			}
 			else if (introSlide == 3)
 			{
-				plane.renderer.material = introMaterial3;
+				materials[1] = introMaterial3;
 			}
 			else if (introSlide == 4)
 			{
+				materials[1] = introMaterial4;
+			}
+			else if (introSlide == 5)
+			{
 				Application.LoadLevel("Main Scene");
 			}
+			
+			plane.renderer.materials = materials;
 		}
 	}
 	
@@ -37,7 +45,9 @@ public class Intro : MonoBehaviour
 		introSlide = 1;
 		
 		GameObject plane = GameObject.Find("Plane");
-		plane.renderer.material = introMaterial1;
+		Material[] materials = plane.renderer.materials;
+		materials[1] = introMaterial1;
+		plane.renderer.materials = materials;
 	}
 	
 	void Update()
